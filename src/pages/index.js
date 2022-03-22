2ada81b93da41e584926485ca2bb7329c0dfbc3c
pa-naki <ryu.tech.try1956@gmail.com>
2022-03-22 17:29:37 +0900

    add: add .md file

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,18 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      
+      <img src="" alt="大和化学紹介写真(一定時間切替)"></img>
+      <img src="" alt="強炭化革命"></img>
+      <div>
+        <div>
+          <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>  
+          </ul>
+        </div>
+      </div>
     </Layout>
   )
 }
